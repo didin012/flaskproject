9bcfffb1e0787ab27d7b7f783cd317e1ed9fa424
Aldrn Astronomo <aldrinastronomo@gmail.com>
2024-02-01 20:21:11 +0800

    updated files part 2 done

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 25, 2023'
+    },
+    {
+        'author': 'Aldrin Astronomo',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'Feb 27, 2023'
+    }
+]
 
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def hello():
-    return "<h1>Hello World!</h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 if __name__ == "__main__":
     app.run(debug=True)
